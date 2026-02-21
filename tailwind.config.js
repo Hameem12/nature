2845b70e2f834c5432a8b2a9865ec3cf85f9dcc9
SAMHH313 <Hashibulhamim@gmail.com>
2026-02-21 19:29:39 +0600

    mobile bg issue

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  darkMode: false, // or 'media' or 'class' – set to false to turn it off
+  theme: {
+    extend: {
+      colors: {
+        'custom-green': '#179800',
+      },
+    },
+  },
+  plugins: [],
+}
